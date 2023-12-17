9ee50ee6737e31a660def1229882f0263fe70fb8
vin <krishanth.cse@gmail.com>
2023-12-17 12:22:05 +0530

    complete the forget password functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from "react-toastify";
+
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
 
   function onChange(e) {
     setEmail(e.target.value);
+  }
+  async function onSubmit(e){
+    e.preventDefault()
+      try{
+        const auth = getAuth()
+        const userCredential = await sendPasswordResetEmail(auth, email);
+        toast.success("Email was sent");
+  
+      }
+      catch(error){
+        toast.error("Could not send reset password");
+      }
   }
 
   return (
@@ -21,7 +36,7 @@ export default function SignIn() {
           />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               className='mb-6 w-full 
               px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 
